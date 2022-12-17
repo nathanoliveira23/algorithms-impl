2202cef4b065a50b765d27db288e7821e453d0c6
nathanoliveira23 <nathanoliveira399@gmail.com>
2022-12-17 15:19:03 -0300

    refact palindrome algorithm

--- palindrome/palindrome.cpp
+++ palindrome/palindrome.cpp
@@ -2,30 +2,11 @@
 #include <string>
 
 int main() {
-    constexpr int size = 7;
     std::string str = "EXAMPLE";
-    char to_rev[size];
+    std::string palindrome = "";
 
-    // SPLIT
-    for (int it = 0; it < size; it++)
-        to_rev[it] = str[it];
-    
-    // REVERSE
-    int i, j = 1;
+    for (int i = str.size(); i >= 0; i--)
+        palindrome += str[i];
 
-    for (i = 0; i < size; i++) {
-        if ((size - j) - i < 0) break;
-
-        char begin = to_rev[i];
-        char end = to_rev[size - j];
-
-        to_rev[i] = end;
-        to_rev[size - j] = begin;
-
-        j++;
-    }
-
-    // SHOW RESULT
-    for (auto item : to_rev)
-        std::cout << item;
+    std::cout << palindrome << std::endl;
 }
